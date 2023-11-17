d4338e563be6293c2ac5f114fd19e738f0541060
Bohdan Vakh <Bohdanvah@gmail.com>
2023-11-17 22:18:49 +0200

    feat/add validation

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,12 @@
 class Appointment < ApplicationRecord
   belongs_to :doctor
   belongs_to :patient
+
+  validate :valid_appointments_count, on: :create
+
+  def valid_appointments_count
+    if doctor.appointments.where.not(recommendation: nil).count <= 10
+      errors.add(:base, "Doctor currently has the maximum number of appointments")
+    end
+  end
 end
