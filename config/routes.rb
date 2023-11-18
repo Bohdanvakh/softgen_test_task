fd1c2181336ae38c84cc0720ced106d10f003c2d
Bohdan Vakh <Bohdanvah@gmail.com>
2023-11-18 16:56:19 +0200

    feat/add show method for patient

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :doctors
+  resources :patients
   resources :appointments
 
   get "/doctors/home", to: 'doctors#index'
