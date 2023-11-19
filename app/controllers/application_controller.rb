6c42c969afed9437a429c94ca88dca5e4b358aa6
Bohdan Vakh <Bohdanvah@gmail.com>
2023-11-19 19:56:51 +0200

    feat/add rescue in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to permission_denied_path, error: exception.message
+  end
+
+  def current_ability
+    user = current_patient || current_doctor
+    @current_ability ||= ::Ability.new(user)
+  end
 end
